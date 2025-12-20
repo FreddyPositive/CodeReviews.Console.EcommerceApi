714598b07fbf670ef5a17f63823bbc94279a0542
FreddyPositive <ferdinandthomas918@gmail.com>
2025-12-20 10:11:47 +0530

    Api App completed

---
+++ freddypositive.JafnaEcommerceApi/Models/DTOs/CategoryDTOs/CategoryCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JafnaEcommerceApi.Models.DTOs.CategoryDTOs;
+
+public class CategoryCreateDto
+{
+    [Required]
+    public string Name { get; set; }
+}
---
+++ freddypositive.JafnaEcommerceApi/Models/DTOs/CategoryDTOs/CategoryUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JafnaEcommerceApi.Models.DTOs.CategoryDTOs;
+
+public class CategoryUpdateDto
+{
+    [Required]
+    public string Name { get; set; }
+}
